30e80fb345e18c70a89396fe241aac100fb37560
Cameron Whitehead <camjw119@gmail.com>
2018-11-12 09:31:35 +0000

    Added test file for reducePeeps

--- src/reducers/reducePeeps/reducePeeps.test.js
+++ src/reducers/reducePeeps/reducePeeps.test.js
@@ -11,4 +11,20 @@ describe('reducePeeps', () => {
   it('should respond to ADD_PEEPS', () => {
     expect(reducePeeps([], Object.assign({}, {type: 'ADD_PEEPS'}, addPeepsState))).toEqual(addPeepsState);
   });
+
+  it('should respond to CLEAR_PEEPS', () => {
+    expect(reducePeeps([], Object.assign({}, {type: 'CLEAR_PEEPS'}, addPeepsState))).toEqual({ isFetching: false, peeps: [] });
+  });
+
+  it('should respond to REQUEST_PEEPS', () => {
+    expect(reducePeeps([], Object.assign({}, {type: 'REQUEST_PEEPS'}, addPeepsState))).toEqual({ isFetching: false, peeps: [{ body: "test" }]});
+  });
+
+  it('should respond to a default option', () => {
+    expect(reducePeeps(addPeepsState, Object.assign({}, {type: 'POST_PEEP'}, addPeepsState))).toEqual(addPeepsState);
+  });
+
+  it('should respond to a default parameters', () => {
+    expect(reducePeeps(undefined, Object.assign({}, {type: 'POST_PEEP'}, addPeepsState))).toEqual([]);
+  });
 });
