c1c458b45b522f7b64c90e05eaba8372b1fc7f96
camjw <camjw119@gmail.com>
2018-11-30 15:09:07 +0000

    Tests passing now, starting linting

--- src/components/ChitterFeed/ChitterFeed.test.js
+++ src/components/ChitterFeed/ChitterFeed.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import ChitterFeed from './ChitterFeed'
 
-const chitterFeedWrapper = shallow(<ChitterFeed peeps={ [{ peep: { body: 'Hello test' } }] }/>)
+const chitterFeedWrapper = shallow(<ChitterFeed peeps={ [{ body: 'Hello test', user: { handle: 'Hello' } }] }/>)
 
 describe('ChitterFeed', () => {
   describe('#render', () => {
     it('renders some HTML', () => {
-      expect(chitterFeedWrapper.html()).toEqual('hello')
+      expect(chitterFeedWrapper.html()).toMatchSnapshot()
     })
   })
 })
