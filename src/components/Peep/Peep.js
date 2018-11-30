1bb7fd858796331981165a416c026b1f223e9d56
camjw <camjw119@gmail.com>
2018-11-30 17:07:22 +0000

    linted Peep class

--- src/components/Peep/Peep.js
+++ src/components/Peep/Peep.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
-const Peep = props => {
-  return (
-    <div className="container">
-      <div className="row">
-        {props.peep.body}
-      </div>
-      <div className="row">
-        Peeped by
-        {' '}
-        {props.peep.user.handle}
-        {' '}
-        {moment(props.peep.created_at).fromNow()}
-.
-      </div>
-      <hr />
-      <br />
+const Peep = ({ peep }) => (
+  <div className="container">
+    <div className="row">
+      {peep.body}
     </div>
-  );
+    <div className="row">
+        Peeped by
+      {' '}
+      {peep.user.handle}
+      {' '}
+      {moment(peep.created_at).fromNow()}
+.
+    </div>
+    <hr />
+    <br />
+  </div>
+);
+
+Peep.propTypes = {
+  peep: PropTypes.shape.isRequired,
 };
 
 export default Peep;
