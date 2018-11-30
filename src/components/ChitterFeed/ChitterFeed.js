801abe1a78939239d1fcaf9f38c15d1a6c9122b1
camjw <camjw119@gmail.com>
2018-11-30 17:07:43 +0000

    Linted ChitterFeed

--- src/components/ChitterFeed/ChitterFeed.js
+++ src/components/ChitterFeed/ChitterFeed.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Peep from '../Peep';
 
-export default class ChitterFeed extends React.Component {
-  render() {
-    return (
-      <ul>
-        <hr />
-        {this.props.peeps.map((peep, index) => (
-          <div key={index}>
-            <Peep peep={peep} />
-          </div>
-        ))}
-      </ul>
-    );
-  }
-}
+const ChitterFeed = ({ peeps }) => (
+  <ul>
+    <hr />
+    {peeps.map(peep => (
+      <div key={peep.id}>
+        <Peep peep={peep} />
+      </div>
+    ))}
+  </ul>
+);
+
+ChitterFeed.propTypes = {
+  peeps: PropTypes.arrayOf(PropTypes.shape),
+};
+
+ChitterFeed.defaultProps = {
+  peeps: [{ body: 'Loading', user: { handle: 'Loading' }, createdAt: 'Loading' }],
+};
+
+export default ChitterFeed;
