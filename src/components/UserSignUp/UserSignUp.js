b3673f68db28ca32f3013685dfb65572c73300a9
camjw <camjw119@gmail.com>
2018-12-02 11:16:28 +0000

    Updated user sign up component

--- src/components/UserSignUp/UserSignUp.js
+++ src/components/UserSignUp/UserSignUp.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './UserSignUp.css'
 
 export default class UserSignUp extends React.Component {
   constructor(props) {
-    super(props);
+    super();
+    const { createUser } = props;
+    this.createNewUser = (handle, password) => createUser(handle, password);
   }
 
   render() {
     return (
-      <h3> Hello </h3>
+      <div className='container'>
+        <div className='row'>
+          <h3>Sign up for Chitter</h3>
+        </div>
+        <div className='row'>
+          <label>User name: </label><input id='userName' type='text' onChange={this.updateUsername}/> <br/>
+          <label>Password: </label><input id='password' type='text' onChange={this.updatePassword}/><br/>
+          <label>Confirm password: </label><input id='passwordConfirm' type='text' onChange={this.updatepasswordConfirm}/><br/>
+        </div>
+        <div className='row'>
+          <button className='btn btn-primary' type="submit" onClick={() => this.createNewUser()}>
+            Sign up
+          </button>
+        </div>
+      </div>
     );
   }
 }
 
-// UserSignUp.propTypes = {
-//   peep: PropTypes.shape().isRequired,
-// };
+UserSignUp.propTypes = {
+  createUser: PropTypes.func.isRequired,
+};
