57383adc76d2c9a28fc2349d08be9ca5de06e3d3
camjw <camjw119@gmail.com>
2018-12-03 15:54:24 +0000

    Container is aware of incorrect sign ups

--- src/containers/UserSignUpContainer/UserSignUpContainer.js
+++ src/containers/UserSignUpContainer/UserSignUpContainer.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import UserSignUp from '../../components/UserSignUp';
-import { createUser } from '../../actions/userActions';
+import { createUser, wrongConfirm } from '../../actions/userActions';
 
 const mapStateToProps = state => ({
   signedIn: state.reduceUser.signedIn,
+  incorrectConfirm: state.reduceUser.incorrectConfirm
 });
-const mapDispatchToProps = { createUser };
+const mapDispatchToProps = { createUser, wrongConfirm };
 const UserSignUpContainer = connect(mapStateToProps, mapDispatchToProps)(UserSignUp);
 
 export default UserSignUpContainer;
